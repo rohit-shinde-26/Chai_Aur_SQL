5b703185777c92c0aeeca46489c0e1901c66950b
Rohit <rohitshinde26072003@gmail.com>
2024-07-20 16:31:55 +0530

    auto increment

--- 1 Introduction/employee.sql
+++ 1 Introduction/employee.sql
@@ -7,7 +7,7 @@ USE rohit;
 -- Check if the database switch was successful
 SELECT DATABASE();
 
--- Create the employee table
+-- Create the employee table if not exists
 CREATE TABLE IF NOT EXISTS employee (
     EID INT,
     FirstName VARCHAR(20) NOT NULL,
@@ -17,8 +17,37 @@ CREATE TABLE IF NOT EXISTS employee (
     PRIMARY KEY (EID)
 );
 
--- Sees the table with all info
-DESC employee;
+-- Drop employee_2 table if it exists
+DROP TABLE IF EXISTS employee_2;
 
--- Check if the table was created successfully
+-- Check if the table was dropped successfully
 SHOW TABLES;
+
+-- Insert values into employee table
+INSERT INTO employee (EID, FirstName, LastName, Age, Location) VALUES (1, 'Ram', 'Charan', 27, 'Bengaluru');
+INSERT INTO employee (EID, FirstName, LastName, Age, Location) VALUES (2, 'Sham', 'Bhosle', 22, 'Hyderabad');
+
+-- See the inserted data in the employee table
+SELECT * FROM employee;
+
+
+
+
+-- if you want to add auto increment then use 
+
+-- CREATE TABLE IF NOT EXISTS employee (
+--     EID INT AUTO_INCREMENT,
+--     FirstName VARCHAR(20) NOT NULL,
+--     LastName VARCHAR(20) NOT NULL,
+--     Age INT NOT NULL,
+--     Location VARCHAR(50) NOT NULL,
+--     PRIMARY KEY (EID)
+-- );
+
+
+-- -- Insert values into employee table
+-- INSERT INTO employee (FirstName, LastName, Age, Location) VALUES ('Ram', 'Charan', 27, 'Bengaluru');
+-- INSERT INTO employee (FirstName, LastName, Age, Location) VALUES ('Sham', 'Bhosle', 22, 'Hyderabad');
+
+-- -- See the inserted data in the employee table
+-- SELECT * FROM employee;
